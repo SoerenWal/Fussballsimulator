dfe9d1e3eb4838166e2c63711b010ae1feca2b44
richard <richard@meine-email.de>
2025-05-06 15:54:11 +0200

    Roboter Changes

--- Fußballsimulator/src/entitaeten/Roboter.java
+++ Fußballsimulator/src/entitaeten/Roboter.java
@@ -1,5 +1,58 @@
 package entitaeten;
 
-public class Roboter {
+public abstract class Roboter {
+	private String name;
+	private int id;
+	private double geschwindigkeit;
+	private int energie;
+	private boolean hatBallBesitz;
+	private int x;
+	private int y;
+    
+    public String getName() {
+    	return this.name
+    }
+    public int getId() {
+    	return this.id
+    }
+    public double getGeschwindigkeit() {
+    	return this.geschwindigkeit
+    }
+    public int getEnergie() {
+    	return this.energie
+    }
+    public boolean getHatBallBesitz() {
+    	return this.hatBallBesitz
+    }
+    public int getX() {
+    	return this.x
+    }
+    public int getY() {
+    	return this.y
+    }
+    
+    public void setName(String name) {
+    	this.name = name;
+    }
+    public void setId(int id) {
+    	this.id = id;
+    }
+    public void setGeschwindigkeit(double geschwindigkeit) {
+    	this.geschwindigkeit = geschwindigkeit;
+    }
+    public void setEnergie(int energie) {
+    	this.energie = energie;
+    }
+    public void setBallBesitz(boolean ballBesitz) {
+    	this.hatBallBesitz = ballBesitz;
+    }
+    public void setX(int x) {
+    	this.x = x;
+    }
+    public void setX(int y) {
+    	this.x = y;
+    }
+
+
 
 }
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Fußballsimulator {
+}
