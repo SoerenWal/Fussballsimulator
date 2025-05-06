5fd6bc22f91f2c9c648c246809cb6749bdf8a8be
NoahMarkanovic <NoahMarkanovic@MDXN01125890.bku.db.de>
2025-05-06 15:51:26 +0200

    aaaaa

--- Fußballsimulator/src/entitaeten/Mittelfeldspieler.java
+++ Fußballsimulator/src/entitaeten/Mittelfeldspieler.java
@@ -1,5 +1,8 @@
 package entitaeten;
 
-public class Mittelfeldspieler {
+public class Mittelfeldspieler extends Roboter {
 
+    public Mittelfeldspieler(String name, int id, double geschwindigkeit, double praezision, int energie) {
+        super(name, id, geschwindigkeit, praezision, energie);
+    }
 }
