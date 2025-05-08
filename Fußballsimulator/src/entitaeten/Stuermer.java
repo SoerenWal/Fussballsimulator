ac0090253b6afc1a5b0df4da4dad492a3eca6b8e
richard <richard@meine-email.de>
2025-05-08 18:26:48 +0200

    Constructor added to Stuermer

--- Fußballsimulator/src/entitaeten/Stuermer.java
+++ Fußballsimulator/src/entitaeten/Stuermer.java
@@ -1,5 +1,13 @@
 package entitaeten;
 
-public class Stuermer {
+public class Stuermer extends Roboter {
+
+	public Stuermer(String name, int id, double geschwindigkeit, double praezision, int energie) {
+        this.setName(name);
+        this.setId(id); 
+        this.setGeschwindigkeit(geschwindigkeit);
+        this.setPraezision(praezision);
+        this.setEnergie(energie);
+    }
 
 }
