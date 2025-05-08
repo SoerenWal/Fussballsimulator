7af02ded95f85aaf63480ab19ebf958bab8e647c
richard <richard@meine-email.de>
2025-05-08 18:32:37 +0200

    Vereidiger Constuctor added

--- Fußballsimulator/src/entitaeten/Verteidiger.java
+++ Fußballsimulator/src/entitaeten/Verteidiger.java
@@ -1,5 +1,13 @@
 package entitaeten;
 
 public class Verteidiger {
+	
+	public Verteidiger(String name, int id, double geschwindigkeit, double praezision, int energie) {
+        this.setName(name);
+        this.setId(id); 
+        this.setGeschwindigkeit(geschwindigkeit);
+        this.setPraezision(praezision);
+        this.setEnergie(energie);
+    }
 
 }
