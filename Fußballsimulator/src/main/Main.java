d517cc3cfcfcd7d813e89680d049104eb4ad44f6
richard <richard@meine-email.de>
2025-05-10 16:06:49 +0200

    Changes

--- Fußballsimulator/src/main/Main.java
+++ Fußballsimulator/src/main/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//Fußballfeld.fußballfeldAnzeigen();
+		Fußballfeld.fußballfeldAnzeigen();
 		 
 		/*Random random = new Random();
 		
