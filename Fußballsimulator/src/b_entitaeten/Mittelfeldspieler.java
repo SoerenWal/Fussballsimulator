70151c1b4bee906f6a900b9b3511f80fd0fb8335
NoahMarkanovic <NoahMarkanovic@MDXN01125890.bku.db.de>
2025-05-11 14:20:59 +0200

    Mittelfeldspieler: Konstruktor passespielen und toString implementiert

--- Fußballsimulator/src/b_entitaeten/Mittelfeldspieler.java
+++ Fußballsimulator/src/b_entitaeten/Mittelfeldspieler.java
@@ -2,12 +2,36 @@ package b_entitaeten;
 
 public class Mittelfeldspieler extends Roboter {
 
-    public Mittelfeldspieler(String name, int id, double geschwindigkeit, double praezisionPass, double prazisionSchuss, int energie) {
+    public Mittelfeldspieler(String name, int id, 
+    		double geschwindigkeit, double praezisionPass, double prazisionSchuss, int energie) {
+    	
         this.setName(name);
         this.setId(id); 
         this.setGeschwindigkeit(geschwindigkeit);
         this.setPraezisionPass(praezisionPass);
         this.setPraezisionSchuss(prazisionSchuss);
         this.setEnergie(energie);
+        
     }
-}
+        
+        
+        public void passeSpielen() {
+            if (this.getEnergie() > 0) {
+                System.out.println(this.getName() + " spielt einen Pass mit Präzision " + this.getPraezisionPass());
+                this.setEnergie(this.getEnergie() - 5); // Energieverbrauch simulieren
+            } else {
+                System.out.println(this.getName() + " hat keine Energie mehr und kann keinen Pass spielen.");
+            }
+        }
+        
+        public String toString() {
+            return "Mittelfeldspieler: " + this.getName() +
+                   ", ID: " + this.getId() +
+                   ", Geschwindigkeit: " + this.getGeschwindigkeit() +
+                   ", Präzision Pass: " + this.getPraezisionPass() +
+                   ", Präzision Schuss: " + this.getPraezisionSchuss() +
+                   ", Energie: " + this.getEnergie();
+        }
+    }
+        
+
