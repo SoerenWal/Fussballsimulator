c06afd837abd5085b7b9ebfc71e1bc27321eb441
richard <richard@meine-email.de>
2025-05-12 15:33:01 +0200

    HelperClass

---
+++ Fußballsimulator/src/b_entitaeten/RoboterHelper.java
@@ -0,0 +1,14 @@
+package b_entitaeten;
+
+public class RoboterHelper {
+
+	public static double praeziPassGenM() {
+		return 0.7 + Math.random() * 0.3;
+	}
+	
+	public static double praeziSchussGenM() {
+		return 
+	}
+}
+
+
