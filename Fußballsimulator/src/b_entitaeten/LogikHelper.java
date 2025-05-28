64b730e08e56487b4c388082c5a1f15f5786aa70
SoerenWalther <SoerenWalther@MDXN01125889.bku.db.de>
2025-05-28 18:35:57 +0200

    tui fix

--- Fußballsimulator/src/b_entitaeten/LogikHelper.java
+++ Fußballsimulator/src/b_entitaeten/LogikHelper.java
@@ -47,13 +47,13 @@ public class LogikHelper {
 	}
 
 	public static void siegerAnzeigen(Mannschaft heimmannschaft, Mannschaft gastmannschaft) {
-		System.out.print(heimmannschaft.name + " hat die Fußball-Partie gegen" + gastmannschaft.name + " mit ");
+		// prüfen ob spiel gestartet
 		if (heimmannschaft.tore > gastmannschaft.tore) {
-			System.out.print(heimmannschaft.name + " hat die Fußball-Partie gegen" + gastmannschaft.name + " mit einem Sieg (" + heimmannschaft.tore + ":" + gastmannschaft.tore + ") beendet.");
+			System.out.print(heimmannschaft.name + " hat die Fußball-Partie gegen " + gastmannschaft.name + " mit einem Sieg (" + heimmannschaft.tore + ":" + gastmannschaft.tore + ") beendet.");
 		} else if (heimmannschaft.tore < gastmannschaft.tore) {
-			System.out.print(gastmannschaft.name + " hat die Fußball-Partie gegen" + heimmannschaft.name + " mit einer Niederlage (" + gastmannschaft.tore + ":" + heimmannschaft.tore + ") beendet.");
+			System.out.print(gastmannschaft.name + " hat die Fußball-Partie gegen " + heimmannschaft.name + " mit einer Niederlage (" + gastmannschaft.tore + ":" + heimmannschaft.tore + ") beendet.");
 		} else {
-			System.out.println(heimmannschaft.name + " hat die Fußball-Partie gegen" + gastmannschaft.name + " mit einem Unentschieden (" + heimmannschaft.tore + ":" + gastmannschaft.tore + " beendet");
+			System.out.println(heimmannschaft.name + " hat die Fußball-Partie gegen " + gastmannschaft.name + " mit einem Unentschieden (" + heimmannschaft.tore + ":" + gastmannschaft.tore + ") beendet.");
 		}
 	}
 }
